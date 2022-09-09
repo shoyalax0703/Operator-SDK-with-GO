cb29259d9b449a55a605b18cd1ba0e324af2745d
shoyalax0703 <shoya.lax@gmail.com>
2022-09-09 16:37:00 +0900

    we will run the Operator as a Go program outside the cluster using Operator-SDK and our kubeconfig credentials

--- api/v1alpha1/podset_types.go
+++ api/v1alpha1/podset_types.go
@@ -1,7 +1,7 @@
 package v1alpha1
 
 import (
-        metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -9,16 +9,16 @@ import (
 
 // PodSetSpec defines the desired state of PodSet
 type PodSetSpec struct {
-        // Replicas is the desired number of pods for the PodSet
-        // +kubebuilder:validation:Minimum=1
-        // +kubebuilder:validation:Maximum=10
-        Replicas int32 `json:"replicas,omitempty"`
+	// Replicas is the desired number of pods for the PodSet
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=10
+	Replicas int32 `json:"replicas,omitempty"`
 }
 
 // PodSetStatus defines the current status of PodSet
 type PodSetStatus struct {
-        PodNames        []string        `json:"podNames"`
-  AvailableReplicas	int32	`json:"availableReplicas"`
+	PodNames          []string `json:"podNames"`
+	AvailableReplicas int32    `json:"availableReplicas"`
 }
 
 // +kubebuilder:object:root=true
@@ -28,22 +28,22 @@ type PodSetStatus struct {
 // +kubebuilder:printcolumn:JSONPath=".spec.replicas",name=Desired,type=string
 // +kubebuilder:printcolumn:JSONPath=".status.availableReplicas",name=Available,type=string
 type PodSet struct {
-        metav1.TypeMeta   `json:",inline"`
-        metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-        Spec   PodSetSpec   `json:"spec,omitempty"`
-        Status PodSetStatus `json:"status,omitempty"`
+	Spec   PodSetSpec   `json:"spec,omitempty"`
+	Status PodSetStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
 // PodSetList contains a list of PodSet
 type PodSetList struct {
-        metav1.TypeMeta `json:",inline"`
-        metav1.ListMeta `json:"metadata,omitempty"`
-        Items           []PodSet `json:"items"`
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PodSet `json:"items"`
 }
 
 func init() {
-        SchemeBuilder.Register(&PodSet{}, &PodSetList{})
+	SchemeBuilder.Register(&PodSet{}, &PodSetList{})
 }
--- controllers/podset_controller.go
+++ controllers/podset_controller.go
@@ -17,27 +17,27 @@ limitations under the License.
 package controllers
 
 import (
-    "context"
-    "reflect"
+	"context"
+	"reflect"
 
-    "k8s.io/apimachinery/pkg/runtime"
-    ctrl "sigs.k8s.io/controller-runtime"
-    "sigs.k8s.io/controller-runtime/pkg/client"
-    ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
-    appv1alpha1 "github.com/redhat/podset-operator/api/v1alpha1"
-    corev1 "k8s.io/api/core/v1"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appv1alpha1 "github.com/redhat/podset-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-    "k8s.io/apimachinery/pkg/api/errors"
-    "k8s.io/apimachinery/pkg/labels"
-    "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 // PodSetReconciler reconciles a PodSet object
 type PodSetReconciler struct {
-    client.Client
-    Scheme *runtime.Scheme
+	client.Client
+	Scheme *runtime.Scheme
 }
 
 //+kubebuilder:rbac:groups=app.example.com,resources=podsets,verbs=get;list;watch;create;update;patch;delete
@@ -54,127 +54,127 @@ type PodSetReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *PodSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-    log := ctrllog.FromContext(ctx)
+	log := ctrllog.FromContext(ctx)
 
-    // Fetch the PodSet instance
-    instance := &appv1alpha1.PodSet{}
-    err := r.Get(context.TODO(), req.NamespacedName, instance)
-    if err != nil {
-        if errors.IsNotFound(err) {
-            // Request object not found, could have been deleted after reconcile request.
-            // Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-            // Return and don't requeue
-            return ctrl.Result{}, nil
-        }
-        // Error reading the object - requeue the request.
-        return ctrl.Result{}, err
+	// Fetch the PodSet instance
+	instance := &appv1alpha1.PodSet{}
+	err := r.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
 
-    }
+	}
 
-    // List all pods owned by this PodSet instance
-    podSet := instance
-    podList := &corev1.PodList{}
-    lbs := map[string]string{
-        "app":     podSet.Name,
-        "version": "v0.1",
-    }
-    labelSelector := labels.SelectorFromSet(lbs)
-    listOps := &client.ListOptions{Namespace: podSet.Namespace, LabelSelector: labelSelector}
-    if err = r.List(context.TODO(), podList, listOps); err != nil {
-        return ctrl.Result{}, err
-    }
+	// List all pods owned by this PodSet instance
+	podSet := instance
+	podList := &corev1.PodList{}
+	lbs := map[string]string{
+		"app":     podSet.Name,
+		"version": "v0.1",
+	}
+	labelSelector := labels.SelectorFromSet(lbs)
+	listOps := &client.ListOptions{Namespace: podSet.Namespace, LabelSelector: labelSelector}
+	if err = r.List(context.TODO(), podList, listOps); err != nil {
+		return ctrl.Result{}, err
+	}
 
-    // Count the pods that are pending or running as available
-    var available []corev1.Pod
-    for _, pod := range podList.Items {
-        if pod.ObjectMeta.DeletionTimestamp != nil {
-            continue
-        }
-        if pod.Status.Phase == corev1.PodRunning || pod.Status.Phase == corev1.PodPending {
-            available = append(available, pod)
-        }
-    }
-    numAvailable := int32(len(available))
-    availableNames := []string{}
-    for _, pod := range available {
-        availableNames = append(availableNames, pod.ObjectMeta.Name)
-    }
+	// Count the pods that are pending or running as available
+	var available []corev1.Pod
+	for _, pod := range podList.Items {
+		if pod.ObjectMeta.DeletionTimestamp != nil {
+			continue
+		}
+		if pod.Status.Phase == corev1.PodRunning || pod.Status.Phase == corev1.PodPending {
+			available = append(available, pod)
+		}
+	}
+	numAvailable := int32(len(available))
+	availableNames := []string{}
+	for _, pod := range available {
+		availableNames = append(availableNames, pod.ObjectMeta.Name)
+	}
 
-    // Update the status if necessary
-    status := appv1alpha1.PodSetStatus{
-        PodNames:          availableNames,
-        AvailableReplicas: numAvailable,
-    }
-    if !reflect.DeepEqual(podSet.Status, status) {
-        podSet.Status = status
-        err = r.Status().Update(context.TODO(), podSet)
-        if err != nil {
-            log.Error(err, "Failed to update PodSet status")
-            return ctrl.Result{}, err
-        }
-    }
+	// Update the status if necessary
+	status := appv1alpha1.PodSetStatus{
+		PodNames:          availableNames,
+		AvailableReplicas: numAvailable,
+	}
+	if !reflect.DeepEqual(podSet.Status, status) {
+		podSet.Status = status
+		err = r.Status().Update(context.TODO(), podSet)
+		if err != nil {
+			log.Error(err, "Failed to update PodSet status")
+			return ctrl.Result{}, err
+		}
+	}
 
-    if numAvailable > podSet.Spec.Replicas {
-        log.Info("Scaling down pods", "Currently available", numAvailable, "Required replicas", podSet.Spec.Replicas)
-        diff := numAvailable - podSet.Spec.Replicas
-        dpods := available[:diff]
-        for _, dpod := range dpods {
-            err = r.Delete(context.TODO(), &dpod)
-            if err != nil {
-                log.Error(err, "Failed to delete pod", "pod.name", dpod.Name)
-                return ctrl.Result{}, err
-            }
-        }
-        return ctrl.Result{Requeue: true}, nil
-    }
+	if numAvailable > podSet.Spec.Replicas {
+		log.Info("Scaling down pods", "Currently available", numAvailable, "Required replicas", podSet.Spec.Replicas)
+		diff := numAvailable - podSet.Spec.Replicas
+		dpods := available[:diff]
+		for _, dpod := range dpods {
+			err = r.Delete(context.TODO(), &dpod)
+			if err != nil {
+				log.Error(err, "Failed to delete pod", "pod.name", dpod.Name)
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
 
-    if numAvailable < podSet.Spec.Replicas {
-        log.Info("Scaling up pods", "Currently available", numAvailable, "Required replicas", podSet.Spec.Replicas)
-        // Define a new Pod object
-        pod := newPodForCR(podSet)
-        // Set PodSet instance as the owner and controller
-        if err := controllerutil.SetControllerReference(podSet, pod, r.Scheme); err != nil {
-            return ctrl.Result{}, err
-        }
-        err = r.Create(context.TODO(), pod)
-        if err != nil {
-            log.Error(err, "Failed to create pod", "pod.name", pod.Name)
-            return ctrl.Result{}, err
-        }
-        return ctrl.Result{Requeue: true}, nil
-    }
+	if numAvailable < podSet.Spec.Replicas {
+		log.Info("Scaling up pods", "Currently available", numAvailable, "Required replicas", podSet.Spec.Replicas)
+		// Define a new Pod object
+		pod := newPodForCR(podSet)
+		// Set PodSet instance as the owner and controller
+		if err := controllerutil.SetControllerReference(podSet, pod, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		err = r.Create(context.TODO(), pod)
+		if err != nil {
+			log.Error(err, "Failed to create pod", "pod.name", pod.Name)
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
 
-    return ctrl.Result{}, nil
+	return ctrl.Result{}, nil
 }
 
 // newPodForCR returns a busybox pod with the same name/namespace as the cr
 func newPodForCR(cr *appv1alpha1.PodSet) *corev1.Pod {
-    labels := map[string]string{
-        "app":     cr.Name,
-        "version": "v0.1",
-    }
-    return &corev1.Pod{
-        ObjectMeta: metav1.ObjectMeta{
-            GenerateName: cr.Name + "-pod",
-            Namespace:    cr.Namespace,
-            Labels:       labels,
-        },
-        Spec: corev1.PodSpec{
-            Containers: []corev1.Container{
-                {
-                    Name:    "busybox",
-                    Image:   "busybox",
-                    Command: []string{"sleep", "3600"},
-                },
-            },
-        },
-    }
+	labels := map[string]string{
+		"app":     cr.Name,
+		"version": "v0.1",
+	}
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: cr.Name + "-pod",
+			Namespace:    cr.Namespace,
+			Labels:       labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "busybox",
+					Image:   "busybox",
+					Command: []string{"sleep", "3600"},
+				},
+			},
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *PodSetReconciler) SetupWithManager(mgr ctrl.Manager) error {
-    return ctrl.NewControllerManagedBy(mgr).
-        For(&appv1alpha1.PodSet{}).
-        Owns(&corev1.Pod{}).
-        Complete(r)
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appv1alpha1.PodSet{}).
+		Owns(&corev1.Pod{}).
+		Complete(r)
 }
